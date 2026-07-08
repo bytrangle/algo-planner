8fd08dbab96bd044893306278adc89ef19370a84
Trang Le <lethutrang101@gmail.com>
2026-07-08 17:03:09 +0700

    add introduction for algo map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -126,14 +126,27 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            AlgoMap
-          </h1>
-          <p>Practice algorithm problems smarter with the power of visualization and AI.</p>
+          <div className='mb-4'>
+            <h1 className="max-w-xs text-4xl font-semibold font-mono leading-10 tracking-tight text-black dark:text-zinc-50">
+              AlgoMap
+            </h1>
+            <p>Practice algorithm problems smarter with the power of visualization and AI.</p>
+          </div>
+          <div className="mb-3">
+            <p className="text-xl">Visualizing all the problems mentioned on <a href="https://labuladong.online/en/algo" className="underline decoration-blue-600">labuladong</a> website and the topics, frameworks and patterns they belong to.</p>
+          </div>
+        </div>
+
+        {/* First div escapes the `max-w-3xl` constraint by the main layout */}
+        <div className="w-screen relative left-1/2 -translate-x-1/2 mb-4">
+          {/* Second div prevents svg from getting too wide on huge screen */}
+          <div className="max-w-7xl mx-auto px-4">
+            <AlgoMap unsolvedSlugs={unsolvedSlugs} />
+          </div>
         </div>
 
         {/* Update Your Map button */}
-        <div className="w-full flex justify-center sm:justify-start mt-8 mb-4">
+        <div className="w-full flex justify-center sm:justify-start mb-4">
           <button
             onClick={() => {
               setUpdateError(null);
@@ -143,14 +156,6 @@ export default function Home() {
           >
             Update Your Map
           </button>
-        </div>
-
-        {/* First div escapes the `max-w-3xl` constraint by the main layout */}
-        <div className="w-screen relative left-1/2 -translate-x-1/2">
-          {/* Second div prevents svg from getting too wide on huge screen */}
-          <div className="max-w-7xl mx-auto px-4 pb-16">
-            <AlgoMap unsolvedSlugs={unsolvedSlugs} />
-          </div>
         </div>
       </main>
 
