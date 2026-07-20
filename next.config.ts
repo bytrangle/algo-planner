e891f5cfab61db06fae6ffd5ae6330a33c28c764
Trang Le <lethutrang101@gmail.com>
2026-07-20 19:55:38 +0700

    remove Next config

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  trailingSlash: true,
-};
-
-export default nextConfig;
